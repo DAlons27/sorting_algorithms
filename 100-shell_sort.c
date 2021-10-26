d4c5d154d28e38d58ec758e472833878a2afa61b
DAlons27 <alonso.morey.arq@gmail.com>
2021-10-25 20:51:49 -0500

    Fix Identation

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,12 +8,12 @@
  */
 size_t get_max_gap(size_t size)
 {
-size_t n;
+    size_t n;
 
-n = 1;
-while (n < size)
-n = n * 3 + 1;
-return ((n - 1) / 3);
+    n = 1;
+    while (n < size)
+        n = n * 3 + 1;
+    return ((n - 1) / 3);
 }
 
 /**
@@ -25,23 +25,23 @@ return ((n - 1) / 3);
  */
 void shell_sort(int *array, size_t size)
 {
-size_t gap, i, j;
-int temp;
+    size_t gap, i, j;
+    int temp;
 
-if (!array || !size)
-return;
+    if (!array || !size)
+        return;
 
-for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
-{
-for (i = gap; i < size; i++)
-{
-temp = array[i];
-for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
-{
-array[j] = array[j - gap];
-}
-array[j] = temp;
-}
-print_array(array, size);
-}
+    for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
+    {
+        for (i = gap; i < size; i++)
+        {
+            temp = array[i];
+            for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
+            {
+                array[j] = array[j - gap];
+            }
+            array[j] = temp;
+        }
+        print_array(array, size);
+    }
 }
