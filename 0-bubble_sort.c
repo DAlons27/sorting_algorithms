b84da83b553a41d501f8d4694209c041615e7004
DAlons27 <alonso.morey.arq@gmail.com>
2021-10-27 21:27:56 -0500

    Fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,25 +8,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    int temp;
-    size_t i, k;
+int temp;
+size_t i, k;
 
-    if (!array || !size)
-        return;
+if (!array || !size)
+return;
 
-    i = 0;
-    while (i < size)
-    {
-        for (k = 0; k < size - 1; k++)
-        {
-            if (array[k] > array[k + 1])
-            {
-                temp = array[k];
-                array[k] = array[k + 1];
-                array[k + 1] = temp;
-                print_array(array, size);
-            }
-        }
-        i++;
-    }
+i = 0;
+while (i < size)
+{
+for (k = 0; k < size - 1; k++)
+{
+if (array[k] > array[k + 1])
+{
+temp = array[k];
+array[k] = array[k + 1];
+array[k + 1] = temp;
+print_array(array, size);
+}
+}
+i++;
+}
 }
